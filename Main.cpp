52ed62ef4bc984f382ee0545132e929e3cf5d80c
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-09-04 15:53:45 +0200

    feat : set up main game loop and integrate classes

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,11 @@
-#include <SFML/Graphics.hpp>
+#include "GameWindow.hpp"
+#include "NumberGrid.hpp"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
+    NumberGrid numberGrid(4);
+    GameWindow gameWindow(numberGrid, 600); // 600x600 window size
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    gameWindow.run();
 
     return 0;
 }
