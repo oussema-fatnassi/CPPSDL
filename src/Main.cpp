0832f1a60e65536a0bf0348c738b118439f2e293
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-04 15:43:46 +0200

    fix : fix problem of tile or background not visible at the same time

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,15 @@
 #include "SDLNumberGrid.hpp"
 #include <SDL.h>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[]) {
-    Window window("2048 Game", 600, 600);
+    Window window("2048 Game", 600, 900);
+    GameObject background = GameObject(window.getRenderer(), "../assets/images/4x4_grid.svg", 0, 0, 450, 450);
+    TTF_Font* font = TTF_OpenFont("../assets/fonts/ClearSansBold.ttf", 24);
+    if (!font) {
+        cerr << "Failed to load font: " << TTF_GetError() << endl;
+    }
 
     SDLNumberGrid grid(window);
 
@@ -10,6 +17,12 @@ int main(int argc, char* argv[]) {
     SDL_Event event;
 
     while (!quit) {
+        window.clear();
+        background.render(window.getRenderer());
+        grid.writeText("2048", font, {255, 255, 0, 255}, 460, 50);
+        grid.render();
+        window.present();
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = true;
@@ -18,7 +31,6 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        grid.render();
 
         if (grid.isGameOver()) {
             quit = true;
@@ -26,6 +38,8 @@ int main(int argc, char* argv[]) {
 
         SDL_Delay(100);
     }
+
+    TTF_CloseFont(font);
 
     return 0;
 }
