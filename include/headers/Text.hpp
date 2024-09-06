7efb93297fbc9fda968c384cd3948c127e349dcd
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-06 12:20:41 +0200

    feat : text class to handle all the text of the game

---
+++ include/headers/Text.hpp
@@ -0,0 +1,25 @@
+#ifndef TEXT_HPP
+#define TEXT_HPP
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <string>
+
+class Text {
+public:
+    Text(SDL_Renderer* renderer, const std::string& text, TTF_Font* font, SDL_Color color, int x, int y);
+    ~Text();
+    void render(SDL_Renderer* renderer);
+    
+private:
+    std::string text;
+    TTF_Font* font;
+    SDL_Color color;
+    SDL_Texture* texture;
+    SDL_Rect rect;
+    SDL_Renderer* renderer;
+
+    void createTexture();
+};
+
+#endif
---
+++ src/Text.cpp
@@ -0,0 +1,38 @@
+#include "Text.hpp"
+#include <iostream>
+
+Text::Text(SDL_Renderer* renderer, const std::string& text, TTF_Font* font, SDL_Color color, int x, int y)
+    : text(text), font(font), color(color), renderer(renderer) {
+
+    rect.x = x;
+    rect.y = y;
+    createTexture();
+}
+
+Text::~Text() {
+    if (texture) {
+        SDL_DestroyTexture(texture);
+    }
+}
+
+void Text::createTexture() {
+    SDL_Surface* textSurface = TTF_RenderText_Blended(font, text.c_str(), color);
+    if (!textSurface) {
+        std::cerr << "Unable to render text surface! TTF_Error: " << TTF_GetError() << std::endl;
+        return;
+    }
+
+    texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    if (!texture) {
+        std::cerr << "Unable to create texture from rendered text! SDL_Error: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    rect.w = textSurface->w;
+    rect.h = textSurface->h;
+    SDL_FreeSurface(textSurface);
+}
+
+void Text::render(SDL_Renderer* renderer) {
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+}
