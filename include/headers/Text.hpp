56b9e3d75c58d855bcb72c3e375bd9641f54231c
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-06 20:19:18 +0200

    feat : add helper function to get and set the text

--- include/headers/Text.hpp
+++ include/headers/Text.hpp
@@ -4,12 +4,15 @@
 #include <../sdl/SDL.h>
 #include <../sdl/SDL_ttf.h>
 #include <string>
+using namespace std;
 
 class Text {
 public:
     Text(SDL_Renderer* renderer, const std::string& text, TTF_Font* font, SDL_Color color, int x, int y);
     ~Text();
     void render(SDL_Renderer* renderer);
+    void setText(const std::string& newText);
+    string getText() { return text; }
     
 private:
     std::string text;
