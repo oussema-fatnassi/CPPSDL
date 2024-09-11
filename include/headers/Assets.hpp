f4f4d44787f9753947d242a116d2c69a8153b295
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-11 17:02:38 +0200

    chore : change file path from svg to png

--- include/headers/Assets.hpp
+++ include/headers/Assets.hpp
@@ -7,9 +7,9 @@ using namespace std;
 // Asset paths for the game
 
 // Button assets
-const string START_BUTTON_NORMAL = "../assets/images/start.svg";
-const string START_BUTTON_HOVER = "../assets/images/start_hover.svg";
-const string START_BUTTON_PRESSED = "../assets/images/start_pressed.svg";
+const string START_BUTTON_NORMAL = "../assets/images/start.png";
+const string START_BUTTON_HOVER = "../assets/images/start_hover.png";
+const string START_BUTTON_PRESSED = "../assets/images/start_pressed.png";
 
 const string QUIT_BUTTON_NORMAL = "../assets/images/quit.svg";
 const string QUIT_BUTTON_HOVER = "../assets/images/quit_hover.svg";
@@ -38,7 +38,7 @@ const string BACK_BUTTON_PRESSED = "../assets/images/back_pressed.svg";
 // Image assets
 const string IMAGE_GRID_3X3 = "../assets/images/image_grid_3x3.png";
 const string IMAGE_GRID_4X4 = "../assets/images/image_grid_4x4.svg";
-const string IMAGE_GRID_5X5 = "../assets/images/image_grid_5x5.svg";
+const string IMAGE_GRID_5X5 = "../assets/images/image_grid_5x5.png";
 const string IMAGE_GRID_6X6 = "../assets/images/image_grid_6x6.svg";
 const string IMAGE_GRID_8X8 = "../assets/images/image_grid_8x8.svg";
 
