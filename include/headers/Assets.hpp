362126383ea8025ace5fe5e56c7a67220e24d9b9
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-12 10:36:33 +0200

    chore : change the assets from svg to png

--- include/headers/Assets.hpp
+++ include/headers/Assets.hpp
@@ -11,47 +11,47 @@ const string START_BUTTON_NORMAL = "../assets/images/start.png";
 const string START_BUTTON_HOVER = "../assets/images/start_hover.png";
 const string START_BUTTON_PRESSED = "../assets/images/start_pressed.png";
 
-const string QUIT_BUTTON_NORMAL = "../assets/images/quit.svg";
-const string QUIT_BUTTON_HOVER = "../assets/images/quit_hover.svg";
-const string QUIT_BUTTON_PRESSED = "../assets/images/quit_pressed.svg";
+const string QUIT_BUTTON_NORMAL = "../assets/images/quit.png";
+const string QUIT_BUTTON_HOVER = "../assets/images/quit_hover.png";
+const string QUIT_BUTTON_PRESSED = "../assets/images/quit_pressed.png";
 
-const string LEFT_ARROW_NORMAL = "../assets/images/left_arrow.svg";
-const string LEFT_ARROW_HOVER = "../assets/images/left_arrow_hover.svg";
-const string LEFT_ARROW_PRESSED = "../assets/images/left_arrow_pressed.svg";
+const string LEFT_ARROW_NORMAL = "../assets/images/left_arrow.png";
+const string LEFT_ARROW_HOVER = "../assets/images/left_arrow_hover.png";
+const string LEFT_ARROW_PRESSED = "../assets/images/left_arrow_pressed.png";
 
-const string RIGHT_ARROW_NORMAL = "../assets/images/right_arrow.svg";
-const string RIGHT_ARROW_HOVER = "../assets/images/right_arrow_hover.svg";
-const string RIGHT_ARROW_PRESSED = "../assets/images/right_arrow_pressed.svg";
+const string RIGHT_ARROW_NORMAL = "../assets/images/right_arrow.png";
+const string RIGHT_ARROW_HOVER = "../assets/images/right_arrow_hover.png";
+const string RIGHT_ARROW_PRESSED = "../assets/images/right_arrow_pressed.png";
 
-const string UNDO_BUTTON_NORMAL = "../assets/images/undo.svg";
-const string UNDO_BUTTON_HOVER = "../assets/images/undo_hover.svg";
-const string UNDO_BUTTON_PRESSED = "../assets/images/undo_pressed.svg";
+const string UNDO_BUTTON_NORMAL = "../assets/images/undo.png";
+const string UNDO_BUTTON_HOVER = "../assets/images/undo_hover.png";
+const string UNDO_BUTTON_PRESSED = "../assets/images/undo_pressed.png";
 
-const string RESTART_BUTTON_NORMAL = "../assets/images/restart.svg";
-const string RESTART_BUTTON_HOVER = "../assets/images/restart_hover.svg";
-const string RESTART_BUTTON_PRESSED = "../assets/images/restart_pressed.svg";
+const string RESTART_BUTTON_NORMAL = "../assets/images/restart.png";
+const string RESTART_BUTTON_HOVER = "../assets/images/restart_hover.png";
+const string RESTART_BUTTON_PRESSED = "../assets/images/restart_pressed.png";
 
-const string BACK_BUTTON_NORMAL = "../assets/images/back.svg";
-const string BACK_BUTTON_HOVER = "../assets/images/back_hover.svg";
-const string BACK_BUTTON_PRESSED = "../assets/images/back_pressed.svg";
+const string BACK_BUTTON_NORMAL = "../assets/images/back.png";
+const string BACK_BUTTON_HOVER = "../assets/images/back_hover.png";
+const string BACK_BUTTON_PRESSED = "../assets/images/back_pressed.png";
 
 // Image assets
 const string IMAGE_GRID_3X3 = "../assets/images/image_grid_3x3.png";
-const string IMAGE_GRID_4X4 = "../assets/images/image_grid_4x4.svg";
+const string IMAGE_GRID_4X4 = "../assets/images/image_grid_4x4.png";
 const string IMAGE_GRID_5X5 = "../assets/images/image_grid_5x5.png";
-const string IMAGE_GRID_6X6 = "../assets/images/image_grid_6x6.svg";
-const string IMAGE_GRID_8X8 = "../assets/images/image_grid_8x8.svg";
+const string IMAGE_GRID_6X6 = "../assets/images/image_grid_6x6.png";
+const string IMAGE_GRID_8X8 = "../assets/images/image_grid_8x8.png";
 
-const string GAME_OVER = "../assets/images/game_over.svg";
-const string GAME_WIN = "../assets/images/game_win.svg";
+const string GAME_OVER = "../assets/images/game_over.png";
+const string GAME_WIN = "../assets/images/game_win.png";
 
-const string GRID = "../assets/images/3x3_grid.svg";
-const string GRID1 = "../assets/images/4x4_grid.svg";
-const string GRID2 = "../assets/images/5x5_grid.svg";
-const string GRID3 = "../assets/images/6x6_grid.svg";
-const string GRID4 = "../assets/images/8x8_grid.svg";
+const string GRID = "../assets/images/3x3_grid.png";
+const string GRID1 = "../assets/images/4x4_grid.png";
+const string GRID2 = "../assets/images/5x5_grid.png";
+const string GRID3 = "../assets/images/6x6_grid.png";
+const string GRID4 = "../assets/images/8x8_grid.png";
 
-const string SCORE = "../assets/images/score.svg";
+const string SCORE = "../assets/images/score.png";
 
 // Font path
 const string FONT_PATH = "../assets/fonts/ClearSansBold.ttf";
